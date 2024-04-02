326e324550879f333acb39a17affee0fd112dd70
Nya Candy <dev@candinya.com>
2024-04-02 14:49:47 +0800

    chore: add process env definition for browser

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { libInjectCss } from "vite-plugin-lib-inject-css";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), libInjectCss()],
+  define: {
+    "process.env.NODE_ENV": '"production"',
+  },
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
